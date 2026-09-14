32e4dbbd033a8d566834f7b706b806770448fed8
Apeki <iamapeki9@gmail.com>
2026-09-14 11:57:26 +0300

    Init commit

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    kotlin("jvm") version "2.4.20"
+    id("com.gradleup.shadow") version "9.6.1"
+}
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
+    compileOnly("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+tasks {
+    build {
+        dependsOn(shadowJar)
+    }
+
+    processResources {
+        val props = mapOf("version" to version)
+        filesMatching("plugin.yml") {
+            expand(props)
+        }
+    }
+}
---
+++ src/main/kotlin/im/apeki/qolmechanics/QoLMechanicsPlugin.kt
@@ -0,0 +1,14 @@
+package im.apeki.qolmechanics
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class QoLMechanicsPlugin : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
